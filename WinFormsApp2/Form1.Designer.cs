c811c5a632be6f477a44acd9460faf1454d5e27a
krisnaarianta <krisnaarianta@gmail.com>
2023-10-26 15:06:23 +0700

    Add project files.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            txtName = new TextBox();
+            txtAlamat = new TextBox();
+            txtNo_handphone = new TextBox();
+            btnInsert = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            btnLoaddata = new Button();
+            dgvData = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvData).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(239, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(124, 25);
+            label1.TabIndex = 0;
+            label1.Text = "DATA USERS";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(61, 106);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(61, 140);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Alamat";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(61, 171);
+            label4.Name = "label4";
+            label4.Size = new Size(90, 15);
+            label4.TabIndex = 3;
+            label4.Text = "No Handphone";
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(167, 103);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(140, 23);
+            txtName.TabIndex = 4;
+            txtName.TextChanged += textBox1_TextChanged;
+            // 
+            // txtAlamat
+            // 
+            txtAlamat.Location = new Point(167, 137);
+            txtAlamat.Name = "txtAlamat";
+            txtAlamat.Size = new Size(140, 23);
+            txtAlamat.TabIndex = 5;
+            // 
+            // txtNo_handphone
+            // 
+            txtNo_handphone.Location = new Point(167, 168);
+            txtNo_handphone.Name = "txtNo_handphone";
+            txtNo_handphone.Size = new Size(140, 23);
+            txtNo_handphone.TabIndex = 6;
+            // 
+            // btnInsert
+            // 
+            btnInsert.Location = new Point(326, 103);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new Size(88, 88);
+            btnInsert.TabIndex = 7;
+            btnInsert.Text = "Insert";
+            btnInsert.UseVisualStyleBackColor = true;
+            btnInsert.Click += btnInsert_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(420, 103);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(88, 88);
+            btnUpdate.TabIndex = 8;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(514, 103);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(88, 88);
+            btnDelete.TabIndex = 9;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnLoaddata
+            // 
+            btnLoaddata.Location = new Point(514, 379);
+            btnLoaddata.Name = "btnLoaddata";
+            btnLoaddata.Size = new Size(88, 37);
+            btnLoaddata.TabIndex = 11;
+            btnLoaddata.Text = "Load Data";
+            btnLoaddata.UseVisualStyleBackColor = true;
+            btnLoaddata.Click += btnLoaddata_Click;
+            // 
+            // dgvData
+            // 
+            dgvData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvData.Location = new Point(61, 208);
+            dgvData.Name = "dgvData";
+            dgvData.RowTemplate.Height = 25;
+            dgvData.Size = new Size(541, 150);
+            dgvData.TabIndex = 12;
+            dgvData.CellClick += dgvData_CellClick;
+            dgvData.CellContentClick += dgvData_CellClick;
+            dgvData.CellContentDoubleClick += dgvData_CellClick;
+            dgvData.CellDoubleClick += dgvData_CellClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(649, 450);
+            Controls.Add(dgvData);
+            Controls.Add(btnLoaddata);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnInsert);
+            Controls.Add(txtNo_handphone);
+            Controls.Add(txtAlamat);
+            Controls.Add(txtName);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgvData).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtName;
+        private TextBox txtAlamat;
+        private TextBox txtNo_handphone;
+        private Button btnInsert;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Button btnLoaddata;
+        private DataGridView dgvData;
+    }
+}
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,190 @@
+using Npgsql;
+using System.Data;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new NpgsqlConnection(connstring);
+        }
+
+        private NpgsqlConnection conn;
+        string connstring = "Host=localhost;Port=5432;Username=postgres;Password=informatika;Database=ListofName";
+
+        public DataTable dt;
+        public static NpgsqlCommand cmd;
+        private string sql = null;
+        private DataGridViewRow r;
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoaddata_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                dgvData.DataSource = null;
+                sql = "select * from st_select()";
+                cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                NpgsqlDataReader rd = cmd.ExecuteReader();
+                dt.Load(rd);
+                dgvData.DataSource = dt;
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "FAIL!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                sql = @"select * from st_insert(:_name, :_alamat, :_no_handphone)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_name", txtName.Text);
+                cmd.Parameters.AddWithValue("_alamat", txtAlamat.Text);
+                cmd.Parameters.AddWithValue("_no_handphone", txtNo_handphone.Text);
+
+                if ((int)cmd.ExecuteScalar() == 1)
+                {
+                    MessageBox.Show("Data Users Berhasil diinputkan", "Well Done!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    conn.Close();
+                    btnLoaddata.PerformClick();
+                    txtName.Text = txtNo_handphone.Text = txtAlamat.Text = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Insert FAIL!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void dgvData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                // Get the selected row
+                r = dgvData.Rows[e.RowIndex];
+
+                // Populate textboxes with the cell values from the selected row
+                txtName.Text = r.Cells["_name"].Value.ToString();
+                txtAlamat.Text = r.Cells["_alamat"].Value.ToString();
+                txtNo_handphone.Text = r.Cells["_no_handphone"].Value.ToString();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (r == null)
+            {
+                MessageBox.Show("Mohon pilih baris data yang akan diupdate", "Good", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            try
+            {
+                conn.Open();
+                sql = @"select * from st_update(:_id, :_name, :_alamat, :_no_handphone)";
+                cmd = new NpgsqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("_id", r.Cells["_id"].Value.ToString());
+                cmd.Parameters.AddWithValue("_name", txtName.Text);
+                cmd.Parameters.AddWithValue("_alamat", txtAlamat.Text);
+                cmd.Parameters.AddWithValue("_no_handphone", txtNo_handphone.Text);
+
+                if ((int)cmd.ExecuteScalar() == 1)
+                {
+                    MessageBox.Show("Data berhasil diupdate", "Well Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    conn.Close();
+                    btnLoaddata.PerformClick();
+                    txtName.Text = txtNo_handphone.Text = txtAlamat.Text = null;
+                    r = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Update FAIL!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+
+        {
+
+            if (r == null)
+
+            {
+
+                MessageBox.Show("Mohon pilih baris data yang akan dihapus", "Warning!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                return;
+
+            }
+
+
+
+            if (MessageBox.Show("Apakah benar Anda ingin menghapus data " + r.Cells["_name"].Value.ToString() + "?", "Hapus Data Terkonfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+
+                try
+
+                {
+
+                    conn.Open();
+
+                    sql = @"select * from st_delete(:_id)";
+
+                    cmd = new NpgsqlCommand(sql, conn);
+
+                    cmd.Parameters.AddWithValue("_id", r.Cells["_id"].Value.ToString());
+
+                    if ((int)cmd.ExecuteScalar() == 1)
+
+                    {
+
+                        MessageBox.Show("Data berhasil di hapus", "Well Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        conn.Close();
+
+                        btnLoaddata.PerformClick();
+
+                        txtName.Text = txtNo_handphone.Text = txtAlamat.Text = null;
+
+                        r = null;
+
+
+
+                    }
+
+                }
+
+                catch (Exception ex)
+
+                {
+
+                    MessageBox.Show("Error: " + ex.Message, "Delete FAIL!!! ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+
+        }
+
+
+
+
+
+
+    }
+}
